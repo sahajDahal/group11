373b1761f2457f365a57ce0ad5a9d496fb4782b6
SalahPlatform <alshalabisalah1@gmail.com>
2025-04-03 09:44:26 -0500

    Update deleteTaskClass.java

--- src/main/java/org/example/deleteTaskClass.java
+++ src/main/java/org/example/deleteTaskClass.java
@@ -1,4 +1,24 @@
-package org.example;
+import java.util.Iterator;
+import java.util.List;
 
 public class deleteTaskClass {
+    private List<Task> tasks;
+
+    public deleteTaskClass(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public boolean deleteTask(String name) {
+        Iterator<Task> iterator = tasks.iterator();
+        while (iterator.hasNext()) {
+            Task task = iterator.next();
+            if (task.getName().equalsIgnoreCase(name)) {
+                iterator.remove();
+                System.out.println("Task deleted: " + task);
+                return true;
+            }
+        }
+        System.out.println("Task not found: " + name);
+        return false;
+    }
 }
